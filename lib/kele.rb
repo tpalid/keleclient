dfab04a1bf91f71afbd31427af8438f64e152e1a
Theresa Palid <tpalid1@gmail.com>
2016-04-27 20:31:30 +0000

    Mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,7 +2,7 @@ require 'httparty'
 require 'json'
 class Kele
     include HTTParty
-    base_uri 'https://www.bloc.io/api/v1'
+    base_uri "https://www.bloc.io/api/v1"
     include JSON
     
     attr_accessor :url, :authtoken
@@ -19,6 +19,11 @@ class Kele
     end
     
     def get_me
-        JSON.parse((self.class.get('/users/me', headers: {"authorization" => @authtoken})).body)
+        @user_data = JSON.parse((self.class.get('/users/me', headers: {"authorization" => @authtoken})).body)
+    end
+    
+    def get_mentor_availability
+        @mentor_id = self.get_me["current_enrollment"]["mentor_id"]
+        @mentor_availability = JSON.parse((self.class.get("/mentors/#{@mentor_id}/student_availability", headers: {"authorization" => @authtoken})).body)
     end
 end
