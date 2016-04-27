faa3bc4742c6d4cde0f24280c2a697956221ea09
Theresa Palid <tpalid1@gmail.com>
2016-04-27 19:26:53 +0000

    Initialization and authorization

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,20 @@
+require 'httparty'
+
 class Kele
-    def initialize(username, password)
-        @username = username
-        @password = password
+    include HTTParty
+    format :json
+    base_uri 'https://www.bloc.io/api/v1'
+    
+    attr_accessor :url, :authtoken
+
+    def initialize(email, password)
+        @url = 'https://www.bloc.io/api/v1'
+        @authtoken = self.sign_in(email, password)
+    end
+    
+    def sign_in(email, password)
+        @authtoken = self.class.post('/sessions', :query => {email: email, password: password})["auth_token"]
+        raise "Invalid login" if @authtoken.nil?
+        @authtoken
     end
 end
